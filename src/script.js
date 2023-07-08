666cba291fe8e43d143aa0f4e2ac2b47189f9ab7
Robson Matos <nosbor_album@hotmail.com>
2023-07-08 20:13:41 -0300

    Commit

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
 function login(){
 	
 }
+
+nameUser = document.getElementById('f-user').value;
+const passPassword = document.getElementById('f-password').value;
+
+const data = [nameUser, passPassword];
+console.log(data);
