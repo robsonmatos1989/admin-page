59e083b41d5f1f7d4fce830a77e23430032fe273
Robson Matos <nosbor_album@hotmail.com>
2023-07-09 21:03:31 -0300

    Armazenando o login em um array.

--- src/script.js
+++ src/script.js
@@ -1,9 +1,12 @@
-function login(){
-	
+function login(event) {
+  	event.preventDefault(); // Impede o envio do formulário
+
+	const nameUser = document.getElementById('f-user').value;
+	const passPassword = document.getElementById('f-password').value;
+
+	const data = [nameUser, passPassword];
+	console.log(data);
+	return;
 }
 
-nameUser = document.getElementById('f-user').value;
-const passPassword = document.getElementById('f-password').value;
 
-const data = [nameUser, passPassword];
-console.log(data);
